3417648ce24b6cf0ad1fd080772032cb9b81cae0
KartoffelCheetah <32495404+KartoffelCheetah@users.noreply.github.com>
2017-12-29 14:47:44 +0100

    Stylus changes.

--- src/app/components/form-fields/open-issues/open-issues.component.ts
+++ src/app/components/form-fields/open-issues/open-issues.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
     selector: 'app-open-issues',
     templateUrl: './open-issues.component.html',
-    styleUrls: ['./open-issues.component.css']
+    styleUrls: ['./open-issues.component.styl']
 })
 export class OpenIssuesComponent implements OnInit {
     // This component lists the Open Issues
